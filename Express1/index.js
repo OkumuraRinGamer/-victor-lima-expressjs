7c7612f45868c9cec7db9126136b6fd58e30fd2e
Pedro Rodrigues <pedroras004@gmail.com>
2021-03-02 21:54:54 -0300

    AULAS8-10

--- Express1/index.js
+++ Express1/index.js
@@ -1,18 +1,33 @@
 const express = require("express");
+
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Seja bem-vindo ao meu app!");
+  res.sendFile(__dirname + "/html/index.html");
 });
 
 app.get("/sobre", function (req, res) {
-  res.send("Minha pagina sobre");
+  res.sendFile(__dirname + "/html/sobre.html");
 });
 
 app.get("/blog", function (req, res) {
   res.send("Bem-vindo ao meu blog!");
 });
 
-app.listen(8081, function () {
-  console.log("Servidor Rodadno na urls http://localhost:8081");
+app.get("/ola/:cargo/:nome/:cor", function (req, res) {
+  res.send(
+    "<h1>Ola " +
+      req.params.nome +
+      "</h1>" +
+      "<h2>Seu cargo e: " +
+      req.params.cargo +
+      "</h2>" +
+      "<h3>Sua cor favorita e: " +
+      req.params.cor +
+      "</h3>"
+  );
+});
+
+app.listen(3000, function () {
+  console.log("Servidor Rodando!");
 });
