551f92e7e1241e9bb6bd87eebdd48e0240df2cae
Pedro Rodrigues <pedroras004@gmail.com>
2021-03-25 20:49:52 -0300

    AULAS25-28

--- Express1/index.js
+++ Express1/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
+
 const Post = require("./models/Post");
 // Config
 // Template Engine
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Body Parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Rotas
 app.get("/", function (req, res) {
-  res.render("home");
+  Post.findAll({ order: [["id", "DESC"]] }).then(function (posts) {
+    res.render("home", { posts: posts });
+  });
 });
 app.get("/cad", function (req, res) {
   res.render("formulario");
@@ -29,6 +31,16 @@ app.post("/add", function (req, res) {
     })
     .catch(function (erro) {
       res.send("Houve um erro: " + erro);
+    });
+});
+
+app.get("/deletar/:id", function (req, res) {
+  Post.destroy({ where: { id: req.params.id } })
+    .then(function () {
+      res.send("Postagem deletada com sucesso!");
+    })
+    .catch(function (erro) {
+      res.send("Esta postagem não existe! ");
     });
 });
 
